9dca74eb4d7eb0781bb86767c79e0009c91877fc
Tyler Landowski <33610663+TylerLandowski@users.noreply.github.com>
2020-10-10 17:13:46 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,343 @@
+from pygame.locals import *
+from datetime import datetime
+
+import random
+import pygame
+
+# Direction IDs
+LEFT = 0
+RIGHT = 1
+UP = 2
+DOWN = 3
+
+class Food:
+	x = 0
+	y = 0
+	rand = 0
+	map_size = 0
+	square_size = 0
+	
+	def __init__(self, is_random, map_size, square_size):
+		self.map_size = map_size
+		self.square_size = square_size
+		self.rand = random.Random()
+		if is_random:
+			self.rand.seed(datetime.now())
+		else:
+			self.rand.seed(0)
+		self.move()
+	
+	def move(self):
+		self.x = self.rand.randrange(self.map_size) * self.square_size
+		self.y = self.rand.randrange(self.map_size) * self.square_size
+
+class Player:
+	x = 0
+	y = 0
+	dir = RIGHT
+	tail = [[0,0]]
+	tail_end = 0
+	map_size = 0
+	square_size = 0
+	
+	def __init__(self, map_size, square_size, verbose):
+		self.x = square_size
+		self.y = 0
+		self.dir = RIGHT
+		self.tail = [[0,0]]
+		self.tail_end = 0
+		self.map_size = map_size
+		self.square_size = square_size
+	
+	def move(self, new_dir):
+		old_x = self.x
+		old_y = self.y
+		invalid_move = 0
+		
+		if new_dir < 0 or new_dir > 3:
+			print("INVALID DIRECTION: " + str(new_dir))
+			return False
+		
+		# Move head
+		if new_dir == UP:
+			new_head = [self.x, self.y - self.square_size]
+			if self.dir == DOWN: return 1
+			elif self.y == 0: return 2
+			elif new_head in self.tail: return 3
+			else: self.moveUp()
+		elif new_dir == DOWN:
+			new_head = [self.x, self.y + self.square_size]
+			if self.dir == UP: return 1
+			elif self.y >= (self.map_size - 1) * self.square_size: return 2
+			elif new_head in self.tail: return 3
+			else: self.moveDown()
+		elif new_dir == LEFT:
+			new_head = [self.x - self.square_size, self.y]
+			if self.dir == RIGHT: return 1
+			elif self.x == 0: return 2
+			elif new_head in self.tail: return 3
+			else: self.moveLeft()
+		elif new_dir == RIGHT:
+			new_head = [self.x + self.square_size, self.y]
+			if self.dir == LEFT: return 1
+			elif self.x >= (self.map_size - 1) * self.square_size: return 2
+			elif new_head in self.tail: return 3
+			else: self.moveRight()
+			
+		# Move tail
+		self.tail[self.tail_end][0] = old_x
+		self.tail[self.tail_end][1] = old_y
+		self.tail_end = (self.tail_end + 1) % len(self.tail)
+		return 4
+
+	def moveRight(self):
+		self.x = self.x + self.square_size
+		self.dir = RIGHT
+
+	def moveLeft(self):
+		self.x = self.x - self.square_size
+		self.dir = LEFT
+
+	def moveUp(self):
+		self.y = self.y - self.square_size
+		self.dir = UP
+
+	def moveDown(self):
+		self.y = self.y + self.square_size
+		self.dir = DOWN
+
+class App:
+	# Clock Speeds
+	VERY_SLOW = 1000
+	SLOW = 500
+	NORMAL = 250
+	FAST = 125
+	VERY_FAST = 50
+	WARP = 25
+	LIGHT = 5
+	NO_CLOCK = 0
+	
+	player = 0
+	verbose = True
+	window_width = 0
+	window_height = 0
+	square_size = 0
+	map_size = 0
+
+	def __init__(self):
+		pass
+		#self._running = True
+		#self._display_surf = None
+		#self._image_head = None
+		#self._image_tail = None
+		#self._image_food = None
+		#self.player = Player()
+		#self.random_food = True
+		#self.moves = 0
+		#self.score = 0
+		#self.verbose = True
+		#self.window_width = 0
+		#self.window_height = 0
+		#self.square_size = 0
+		#self.map_size = 0
+
+	def on_init(self):
+		if self.clock != 0:
+			#print("GOT HERE!")
+			# pygame.init()
+			self._display_surf = pygame.display.set_mode((self.window_width,self.window_height), pygame.HWSURFACE)
+			
+			pygame.display.set_caption('Snake')
+			self._running = True
+			self._image_head = pygame.image.load("head.png").convert()
+			self._image_tail = pygame.image.load("tail.png").convert()
+			self._image_food = pygame.image.load("food.png").convert()
+ 
+	def on_event(self, event):
+		if event.type == QUIT:
+			self._running = False
+	
+	def on_render(self):
+		if self.clock != 0:
+			self._display_surf.fill((255,255,255))
+			self._display_surf.blit(self._image_head,(self.player.x, self.player.y))
+			for i in range(0, len(self.player.tail)):
+				self._display_surf.blit(self._image_tail,(self.player.tail[i][0], self.player.tail[i][1]))
+			self._display_surf.blit(self._image_food,(self.food.x, self.food.y))
+			pygame.display.flip()
+ 
+	def on_cleanup(self, result):
+		# pygame.quit()
+		self.reset()
+		self._running = False
+		print(result)
+		
+	def reset(self, ret_code, result = ""):
+		ret = [ret_code, self.score, self.moves]
+		self.score = 0
+		self.moves = 0
+		self._running = False
+		self.player = Player(self.map_size, self.square_size, self.verbose)
+		self.food = Food(self.random_food, self.map_size, self.square_size)
+		if result != "" and self.verbose == True: print(result)
+		pygame.time.wait(self.wait_on_end)
+		return ret
+		
+	# Starts the game. Useful for playing using the step() function
+	def start (
+		self,
+		clock = NORMAL,
+		map_size = 10,
+		square_size = 20,
+		random_food = True,
+		end_on_collisions = True,
+		wait_on_end = 1500,
+		verbose = True,
+		grow_length = 1
+	):
+		self.moves = 0
+		self.score = 0
+		self.clock = clock
+		self.map_size = map_size
+		self.square_size = square_size
+		self.random_food = random_food
+		self.end_on_collisions = end_on_collisions
+		self.wait_on_end = wait_on_end
+		self.verbose = verbose
+		self.window_width = map_size * square_size
+		self.window_height = map_size * square_size
+		self.grow_length = grow_length
+		self.player = Player(map_size, square_size, verbose)
+		self.food = Food(random_food, map_size, square_size)
+		if self.clock != 0:
+			#pygame.init()
+			self._display_surf = pygame.display.set_mode((self.window_width, self.window_height), pygame.HWSURFACE)
+			
+			pygame.display.set_caption('Snake')
+			self._image_head = pygame.image.load("head.png").convert()
+			self._image_tail = pygame.image.load("tail.png").convert()
+			self._image_food = pygame.image.load("food.png").convert()
+		self._running = True
+		
+	def step (
+		self,
+		action
+	):
+		# ==============
+		# Perform action
+		# ==============
+		
+		invalid_move = self.player.move(action)
+		self.moves += 1
+		# Update screen
+		self.on_render()
+		
+		# =================================
+		# Check collisions / end conditions
+		# =================================
+		
+		# Invalid action
+		if invalid_move == 1 and self.verbose:
+			print("INVALID MOVE - FROM " + str(self.player.dir) + " TO " + str(action))
+		
+		# Wall collision
+		if invalid_move == 2 and self.end_on_collisions:
+			return self.reset(-3, "WALL COLLISION")
+		
+		# Tail collision
+		player_pos = [self.player.x, self.player.y]
+		if invalid_move == 3 and self.end_on_collisions:
+			return self.reset(-3, "SELF COLLISION")
+			
+		# Snake is stuck - all moves result in death
+		square_L = self.player.x == 0 or \
+			[self.player.x - self.square_size, self.player.y] in self.player.tail
+		square_R = self.player.x == (self.map_size - 1) * self.square_size or \
+			[self.player.x + self.square_size, self.player.y] in self.player.tail
+		square_U = self.player.y == 0 or \
+			[self.player.x, self.player.y - self.square_size] in self.player.tail
+		square_D = self.player.y == (self.map_size - 1) * self.square_size or \
+			[self.player.x, self.player.y + self.square_size] in self.player.tail
+		if square_L and square_R and square_U and square_D:
+			return self.reset(-4, "PLAYER STUCK")
+		
+		# Food collision
+		food_pos = [self.food.x, self.food.y]
+		if player_pos == food_pos:
+			self.eat_food()
+		
+		# Check if Escape pressed
+		if self.clock != 0:
+			pygame.event.pump()
+			keys = pygame.key.get_pressed() 
+			if (keys[K_ESCAPE]): return self.reset(-1, "ESCAPE PRESSED")
+			
+		# Check if player won
+		if len(self.player.tail) == self.map_size * self.map_size - 1:
+			return self.reset(0, "SUCCESS!")
+			
+		# ============
+		# Advance game
+		# ============
+		
+		# Wait (unless move did nothing)
+		if invalid_move == 4: pygame.time.wait(self.clock)
+		
+		# Out of moves
+		return [-2, self.score, self.moves];
+	
+	# Run the game as normal, with inputted actions
+	def run (
+		self,
+		actions,
+		clock = NORMAL,
+		map_size = 10,
+		square_size = 20,
+		random_food = True,
+		end_on_collisions = True,
+		wait_on_end = 1500,
+		verbose = True,
+		grow_length = 1
+	):
+		self.start(clock, map_size, square_size, random_food, end_on_collisions, wait_on_end, verbose, grow_length)
+		
+		new_actions = list()
+		
+		if clock != 0:
+			pygame.event.pump()
+			self.on_render()
+			pygame.time.wait(clock)
+		
+		ret = 0
+		breaked = False
+		for i in range(0, len(actions)):
+			ret = self.step(actions[i])
+			if ret[0] != -3:
+				new_actions.append(actions[i])
+			if ret[0] != -2:
+				na_len = len(new_actions)
+				for i in range(na_len, len(actions)):
+					new_actions.append(-1)
+				ret.append(new_actions)
+				return ret
+		
+		# Out of moves
+		na_len = len(new_actions)
+		for i in range(na_len, len(actions)):
+			new_actions.append(-1)
+		ret = self.reset(-2, "OUT OF MOVES")
+		ret.append(new_actions)
+		return ret
+	
+	def eat_food(self):
+		self.score += 1
+		self.food.move()
+		for i in range(0, self.grow_length):
+			self.player.tail.append(self.player.tail[0].copy())
+		
+	#
+	# Useful functions for learning
+	#
+	
+	def manhattan_dist(self):
+		return int((abs(self.player.x - self.food.x) + abs(self.player.y - self.food.y)) / self.square_size)
---
+++ snake.py
@@ -0,0 +1,310 @@
+""" TODO
+	-Write operators that work for random food locations
+		-Fitness: Take minimum across 10+ runs (prioritize solutions which work
+		          generally
+	-Allow game to return a new list of actions that skips over invalid actions,
+	 then replace the individual with that
+	-Adjust mutation to replace more actions when the score is higher - 
+	 higher score = longer length = more actions required to get untrapped
+	-Genome generation:
+		?Look at prev 20 actions to disallow running into walls
+
+"""
+
+"""
+    Genetic algorithm template for the snake game
+    
+    
+    Implemented using DEAP
+    
+    
+	Teresa Muellenbach
+	Nickolas Hexum
+	Tyler Landowski
+	Jamie Vue	
+    November 2019
+    
+"""
+
+
+import random
+import numpy
+import game
+import math
+
+from datetime import datetime
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+SHOW_TRAINING = False
+
+# Problem parameters
+# .9 mpb
+# to_replace = 1, 32, 1
+# actions = 400
+
+# num_spaces = GRID_SIZE^2
+GRID_SIZE = 5
+# number of actions/alleles
+NUM_ACTIONS = 300
+# number of directions in a given individual
+NUM_DIRECT = 4
+# whether food positions are random or not
+RANDOM_FOOD = False
+# end game as soon as snake dies, or skip poor actions (still counts as move)
+END_ON_COLLISIONS = False
+# Fitness penalty for every action taken
+TIME_PENALTY = 0.001
+
+# algorithm parameters
+CXPB = 0.0                 # probability that two selected individuals will recombine
+GENS = 100  		 	   # number of generations in the run
+POP_SIZE = 100       	   # number of individuals
+ELITE_NUM = 10
+TOURN_SIZE = 7
+HOF_SIZE = 5               # number of best members in hall of fame
+MUTPB = 1.0                # unused
+
+# Start the snake game
+app = game.App()
+
+
+# print column headings for the output log
+def print_logbook_header():
+    print("{:>6}{:>8}{:>12}{:>12}{:>12}{:>12}".format("gen", "nevals", "avg", "std", "min", "max"))
+    
+
+# print a single data row from the output log
+# one row represents one generation of the GA run
+def print_logbook_row(r):
+    print("{:>6}{:>8}{:>12.4}{:>12.4}{:>16}{:>16}".format(r['gen'], r['nevals'],
+                                                        r['Avg'], r['Std'],
+                                                        r['Min'], r['Max']))
+
+# Replace the move that ended in death, and a number of moves before that
+def mutation_replace_end(individual):
+	# Get last action
+	last_action = len(individual) - 1
+	for i in range(0, len(individual)):
+		if individual[i] == -1:
+			last_action = i
+	
+	ind_fit = 4
+	if individual.fitness.values: ind_fit = int(individual.fitness.values[0])
+	
+	to_replace = random.randrange(int(ind_fit / 2), 3 * ind_fit, 1)
+	replace_idx = max(last_action - to_replace, 0)
+	replace_rng = len(individual) - replace_idx
+	
+	genome = get_genome(replace_rng)
+	
+	for i in range(replace_idx, len(individual), 1):
+		individual[i] = genome[i - replace_idx]
+		
+	return individual,
+
+def mutation(individual, indpb=-1):
+	return individual,
+
+def mutation_null(individual, indpb):
+	pass
+	#return individual,
+
+def crossover_null(ind1, ind2):
+	return (ind1, ind2)
+
+def get_direct():
+	return random.randrange(NUM_DIRECT)
+
+def get_genome(num_alleles = NUM_ACTIONS):
+	genome = list()
+	
+	LEFT = 0
+	RIGHT = 1
+	UP = 2
+	DOWN = 3
+	
+	# Generate valid move
+	# There should never be a 0 and 1, or a 2 and 3, adjacent anywhere in the
+	#     genome
+	prev_action = RIGHT
+	for i in range(0, num_alleles):
+		new_action = random.randrange(NUM_DIRECT - 1)
+		
+		if prev_action == DOWN and new_action == UP: new_action = DOWN
+		#if prev_action == UP: pass
+		elif prev_action == LEFT and new_action == RIGHT: new_action = DOWN
+		elif prev_action == RIGHT and new_action == LEFT: new_action = DOWN
+		
+		prev_action = new_action
+		genome.append(new_action)
+	
+	return genome
+	
+def evaluate(individual):
+	clock = app.NO_CLOCK
+	if SHOW_TRAINING: clock = app.LIGHT
+	result = app.run(
+		actions = list(individual), # Check this line for performance
+		clock = clock,
+		map_size = GRID_SIZE,
+		random_food = RANDOM_FOOD,
+		end_on_collisions = END_ON_COLLISIONS,
+	    verbose = False,
+        wait_on_end = 0
+	)
+	score = result[1]
+	moves = result[2]
+	individual = result[3]
+	
+	# Distance from food (from 1 to (GRID_SIZE-1)*2)
+	dist_from_food = app.manhattan_dist()
+	bonus = 1 - (dist_from_food / ((GRID_SIZE - 1) * 2))
+	penalty = moves * TIME_PENALTY
+	
+	# Also, mark every action not taken with -1
+	#for i in range(moves, len(individual)):
+		#individual[i] = -1
+	
+	return (score + bonus - penalty,)
+
+# main function
+def snake(population, toolbox, cxpb, mutpb, ngen, stats=None,
+             halloffame=None, verbose=False):
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    for ind in population:
+        del ind.fitness.values
+    fitnesses = toolbox.map(toolbox.evaluate, population)
+    for ind, fit in zip(population, fitnesses):
+        ind.fitness.values = fit
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(population), **record)
+
+    if verbose:
+        print_logbook_header()
+        print_logbook_row(logbook[0])
+    
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        
+        # Select individuals to serve as parents by copying current parent population
+        offspring = toolbox.clone(population)
+        
+        # use the defined select method to choose parents.
+        offspring = toolbox.clone((toolbox.select(population)))
+        
+        # crossover
+        for child1, child2 in zip(offspring[0::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+                
+        # mutation
+        for mutant in offspring:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+        
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+        
+        # replacement
+        #currently chooses ELITE_NUM best individuals and fills rest of
+        #next_pop with individuals chosen at random
+        population.extend(offspring)
+        next_pop = toolbox.replace(population, POP_SIZE - ELITE_NUM)
+        elite = toolbox.elite(population, ELITE_NUM)
+        next_pop.extend(elite)
+        population = toolbox.clone(next_pop)
+        
+        random.shuffle(population)
+        
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        
+        if verbose:
+            print_logbook_row(logbook[gen])
+
+    return population, logbook
+
+
+# YOU WILL CHANGE EVERYTHING IN THIS SECTION
+# create the DEAP structures
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+# THIS IS WHERE YOU CREATE THE REPRESENTATION
+#Structure initializers
+toolbox.register("direction", get_direct)
+toolbox.register("genome", get_genome)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.genome)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("evaluate", evaluate)
+#toolbox.register("mate", tools.cxOnePoint)
+toolbox.register("mate", crossover_null)
+#toolbox.register("mutate", mutation_null, indpb=0.75/NUM_ACTIONS)
+toolbox.register("mutate", mutation_replace_end)
+#toolbox.register("mutate", mutation, indpb=0.75/NUM_ACTIONS)
+# method for parent selection
+toolbox.register("select", tools.selTournament, k=POP_SIZE, tournsize=TOURN_SIZE)
+# method for survival selection
+toolbox.register("replace", tools.selRandom)
+toolbox.register("elite", tools.selBest)
+
+
+def main(seed):
+    random.seed(seed)
+
+    pop = toolbox.population(n=POP_SIZE)
+    hof = tools.HallOfFame(HOF_SIZE)
+    # UPDATE PARAMETER IN FOLLOWING LINE AS NECESSARY
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("Avg", numpy.mean)
+    stats.register("Std", numpy.std)
+    stats.register("Min", numpy.min)
+    stats.register("Max", numpy.max)
+
+    snake(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=GENS, stats=stats,
+                                                            halloffame=hof, verbose=True)
+    return pop, stats, hof
+
+
+if __name__ == "__main__":
+    # call to main program
+    # change the parameter (seed for random) to a constant for repeatability
+    _, _, hof = main(datetime.now())
+    
+    # output individuals from hall of fame
+    for indiv in hof:
+        #print('{}\n'.format(indiv))
+        print("Showing best individuals...")
+        result = app.run(
+            actions = indiv,
+            clock = app.WARP,
+            map_size = GRID_SIZE,
+            random_food = RANDOM_FOOD,
+            end_on_collisions = END_ON_COLLISIONS,
+            wait_on_end = 1000,
+            verbose = True
+        )
